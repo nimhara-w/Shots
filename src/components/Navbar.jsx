567420853609065c6e0566e90db5e359ec4fa603
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-01 16:47:47 +0530

    finished the initial navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import { navLinks } from "../../constants/index.js";
-import icon from "../assets/images/icon.png";
 
 const Navbar = () => {
   return (
     <nav className="w-full py-5 px-10">
       <div className="flex flex-row justify-start items-center gap-10">
-        <a href="#home">
-          <img src={icon} alt="logo" className="w-10 h-10" />
-          <p className="font-gothic text-white text-xl"> Shots </p>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/icon.png" alt="logo" className="w-10 h-10" />
+          <p className="font-gothic text-white text-2xl"> Shots </p>
         </a>
 
-        <ul className="font-base flex flex-row items-center gap-6">
+        <ul className="font-base flex flex-row items-center gap-10">
           {navLinks.map((link) => (
             <li key={link.id}>
               <a href={`#${link.id}`}>{link.title}</a>
