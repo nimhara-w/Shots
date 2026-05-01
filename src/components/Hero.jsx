1b9dacbc9ade220cda3c0a22023035d54bd72532
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-01 17:33:58 +0530

    troubleshooting bg image (status:failed)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <>
-      <section id="hero" className="noise">
+      <section id="hero" className="noise" className="relative z-0">
         <h1 className="font-gothic text-white text-5xl leading-none">
           More Than Coffee
         </h1>
