84907d68491b62a4ca85b6fbed3caa96856d2ae4
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-01 18:07:41 +0530

    hero section base complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 const Hero = () => {
   return (
     <>
-      <section id="hero" className="noise" className="relative z-0">
-        <h1 className="font-gothic text-white text-5xl leading-none">
-          More Than Coffee
+      <section
+        id="hero"
+        className="relative z-0 min-h-dvh w-full border border-transparent"
+      >
+        <div className="noise"></div>
+
+        <h1 className="text-gradient font-bold font-base text-shadow-2xs md:mt-32 mt-40 text-8xl md:text-[10vw] leading-none text-center">
+          Every Shot Counts
         </h1>
       </section>
     </>
