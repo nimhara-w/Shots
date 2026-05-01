786b9025ee5fc321c7601efe4063d4becde74cec
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-01 16:44:29 +0530

    movement of icon to public folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { navLinks } from "../../constants/index.js";
+import icon from "../assets/images/icon.png";
 
 const Navbar = () => {
   return (
     <nav className="w-full py-5 px-10">
       <div className="flex flex-row justify-start items-center gap-10">
         <a href="#home">
+          <img src={icon} alt="logo" className="w-10 h-10" />
           <p className="font-gothic text-white text-xl"> Shots </p>
         </a>
 
