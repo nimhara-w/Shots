81d216aa090171c1684dddb874dedddb1b6a6676
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-01 18:31:53 +0530

    change of header position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       >
         <div className="noise"></div>
 
-        <h1 className="text-gradient font-bold font-base text-shadow-2xs md:mt-32 mt-40 text-8xl md:text-[10vw] leading-none text-center">
+        <h1 className="text-gradient font-bold font-base text-shadow-2xs md:mt-15 mt-20 text-8xl md:text-[10vw] leading-none text-center">
           Caffienated.
         </h1>
       </section>
