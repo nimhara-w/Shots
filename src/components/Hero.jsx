9f3b032597511a77ece6526b770d437ed57ee625
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-02 14:39:22 +0530

    tried and failed to add text animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,10 @@ const Hero = () => {
       >
         <div className="noise"></div>
 
-        <h1 className="text-gradient font-bold font-base text-shadow-2xs md:mt-15 mt-20 text-8xl md:text-[10vw] leading-none text-center">
+        <h1
+          id="title"
+          className="text-gradient font-bold font-base text-shadow-2xs md:mt-15 mt-20 text-8xl md:text-[10vw] leading-none text-center"
+        >
           Caffienated.
         </h1>
       </section>
