87f8bbdb95dd5d1b5bd1dc6eff352827d2f9ad73
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-02 15:16:47 +0530

    routine commit: video still not working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,32 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({ mathWidth: 767 });
+  useGSAP(() => {
+    const startValue = "center 60%";
+    const endValue = "bottom top";
+
+    videoTimelineRef.current = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    if (videoRef.current) {
+      videoRef.current.onloadedmetadata = () => {
+        videoTimelineRef.current.to(videoRef.current, {
+          currentTime: videoRef.current.duration,
+        });
+      };
+    }
+  }, []);
+
   return (
     <>
       <section
