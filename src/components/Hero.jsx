75946281db6ebd85417c45c39732873e34add6fd
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-02 15:37:46 +0530

    removal/restart animated video proccess

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,31 +2,6 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
-  const videoRef = useRef();
-  const isMobile = useMediaQuery({ mathWidth: 767 });
-  useGSAP(() => {
-    const startValue = "center 60%";
-    const endValue = "bottom top";
-
-    videoTimelineRef.current = gsap.timeline({
-      scrollTrigger: {
-        trigger: "video",
-        start: startValue,
-        end: endValue,
-        scrub: true,
-        pin: true,
-      },
-    });
-
-    if (videoRef.current) {
-      videoRef.current.onloadedmetadata = () => {
-        videoTimelineRef.current.to(videoRef.current, {
-          currentTime: videoRef.current.duration,
-        });
-      };
-    }
-  }, []);
-
   return (
     <>
       <section
@@ -42,16 +17,6 @@ const Hero = () => {
           Caffienated.
         </h1>
       </section>
-
-      <div className="video absolute inset-0">
-        <video
-          ref={videoRef}
-          src="/main.mp4"
-          muted
-          playsInline
-          preload="auto"
-        />
-      </div>
     </>
   );
 };
