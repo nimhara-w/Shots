a2afc7debcd6421d61e0be2b0876ab4ad38890be
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-09 18:37:03 +0530

    routine commit

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -67,26 +67,36 @@ const Reviews = () => {
       <h2 className="text-4xl text-gradient font-gothic text-center mb-10">
         What Our Customers Say
       </h2>
-      <div className="w-3/4 m-auto">
-        <div className="mt-20">
+      <div className="w-3/5 m-auto">
+        <div className="mt-12">
           <Slider {...settings}>
             {reviewers.map((reviewer, index) => (
               <div
                 key={index}
                 className="bg-white h-[450px] text-soft-black rounded-xl"
               >
-                <div className="h-56 rounded-t-xl bg-indigo-950 flex justify-center">
+                <div
+                  className="h-56 rounded-t-xl flex justify-center items-center relative overflow-hidden"
+                  style={{
+                    backgroundImage: `url(${reviewer.photo})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                >
+                  <div className="absolute inset-0 bg-smoky/70 backdrop-blur-sm" />
                   <img
                     src={reviewer.photo}
                     alt={reviewer.name}
-                    className="h-44 w-44 rounded-full"
+                    className="h-40 w-40 rounded-full relative z-10 object-cover border-2 border-white/20"
                   />
                 </div>
 
                 <div className="flex flex-col justify-center items-center gap-4 p-4">
-                  <p className="text-xl font-base">{reviewer.name}</p>
-                  <p>{reviewer.review}</p>
-                  <button className="bg-indigo-950 text-white text-lg py-2 px-4 rounded-xl">
+                  <p className="text-2xl font-base">{reviewer.name}</p>
+                  <p className="text-sm font-base text-center">
+                    {reviewer.review}
+                  </p>
+                  <button className="bg-smoky text-white text-lg py-2 px-4 rounded-xl">
                     Read More
                   </button>
                 </div>
