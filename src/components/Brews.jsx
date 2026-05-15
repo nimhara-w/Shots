54f1072c673151ebf658ff734e464a8163b14245
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-05-15 16:36:14 +0530

    media query for BREWS

--- src/components/Brews.jsx
+++ src/components/Brews.jsx
@@ -4,7 +4,7 @@ const Brews = () => {
   return (
     <section
       id="brews"
-      className="w-full h-screen flex items-center justify-center  bg-soft-black/90"
+      className="w-full min-h-screen flex items-center justify-center  bg-soft-black/90 md: py-16 px-5"
     >
       <div className="list">
         <div className="popular">
@@ -16,7 +16,7 @@ const Brews = () => {
             {brewList.map(({ name, description, price }) => (
               <li
                 key={name}
-                className="ml-12 mb-4 m-px-4 flex items-start gap-4 flex-wrap "
+                className="ml-12 mb-4 m-px-4 flex items-start gap-4 flex-wrap md:gap-5 "
               >
                 <div className="md:me-28">
                   <h3 className="text-3xl font-base text-white"> {name} </h3>
